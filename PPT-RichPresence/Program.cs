b468efbc391b6e834d2b20080989328033ce37ce
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-01-28 18:40:03 +0100

    Create Timer and Game Reader

--- PPT-RichPresence/Program.cs
+++ PPT-RichPresence/Program.cs
@@ -3,15 +3,32 @@ using System.Windows.Forms;
 
 namespace PPT_RichPresence {
     static class Program {
+        static ProcessMemory PPT = new ProcessMemory("puyopuyotetris");
+
         static NotifyIcon tray = new NotifyIcon {
             Icon = Properties.Resources.TrayIcon,
             Text = "Puyo Puyo Tetris Rich Presence",
             Visible = true
         };
 
+        static Timer ScanTimer = new Timer {
+            Enabled = true,
+            Interval = 1000
+        };
+
+        static void Loop(object sender, EventArgs e) {
+            if (PPT.CheckProcess()) {
+                PPT.TrustProcess = true;
+
+                // Game reads, update presence
+
+                PPT.TrustProcess = false;
+            }
+        }
+
         [STAThread]
-        static void Main() {            
-            while ( true) ;
+        static void Main() {
+            ScanTimer.Tick += new EventHandler(Loop);
         }
     }
 }
