88ab261397e6d3c925200f652cab511ec930eb32
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-01-28 18:30:49 +0100

    Initial commit

---
+++ PPT-RichPresence/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PPT_RichPresence {
+    static class Program {
+        static NotifyIcon tray = new NotifyIcon {
+            Icon = Properties.Resources.TrayIcon,
+            Text = "Puyo Puyo Tetris Rich Presence",
+            Visible = true
+        };
+
+        [STAThread]
+        static void Main() {            
+            while ( true) ;
+        }
+    }
+}
