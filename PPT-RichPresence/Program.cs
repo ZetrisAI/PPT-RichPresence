dab713815af8f8204c3c286d585a338ac83b3d70
mat1jaczyyy <mat1jaczyyy@gmail.com>
2019-01-29 17:33:19 +0100

    Tray Context Menu, Copy Invite Link

--- PPT-RichPresence/Program.cs
+++ PPT-RichPresence/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Text;
 using System.Threading;
 
 using DiscordRPC;
@@ -10,10 +9,33 @@ namespace PPT_RichPresence {
         static DiscordRpcClient Presence;
 
         static System.Windows.Forms.NotifyIcon tray = new System.Windows.Forms.NotifyIcon {
+            ContextMenu = new System.Windows.Forms.ContextMenu(new System.Windows.Forms.MenuItem[] {
+                new System.Windows.Forms.MenuItem("Copy Invite Link", new EventHandler(CopyInviteLink)),
+                new System.Windows.Forms.MenuItem("-"),
+                new System.Windows.Forms.MenuItem("Exit", new EventHandler(Close))
+            }),
             Icon = Properties.Resources.TrayIcon,
             Text = "Puyo Puyo Tetris Rich Presence",
             Visible = true
         };
+
+        static void CheckFreePlayLobby(object sender, EventArgs e) {
+            ((System.Windows.Forms.ContextMenu)sender).MenuItems[0].Enabled = GameHelper.GetMenu() == 28;
+        }
+
+        static void CopyInviteLink(object sender, EventArgs e) {
+            int? menuId = GameHelper.GetMenu();
+
+            if (menuId.HasValue) {
+                if (menuId == 28 /* Free Play Lobby */) {
+                    System.Windows.Forms.Clipboard.SetText(GameHelper.LobbyInvite());
+                }
+            }
+        }
+
+        static void Close(object sender, EventArgs e) {
+            System.Windows.Forms.Application.Exit();
+        }
 
         static Timer ScanTimer = new Timer(new TimerCallback(Loop), null, Timeout.Infinite, 1000);
 
@@ -29,19 +51,11 @@ namespace PPT_RichPresence {
                     },
                 };
 
-                if (menuId == 27 /* Puzzle League Lobby */) ret.Party = new Party() {
+                if (menuId == 27 /* Puzzle League Lobby */ || menuId == 28 /* Free Play Lobby */) ret.Party = new Party() {
                     ID = Guid.Empty.ToString(),
                     Size = GameHelper.LobbySize(),
-                    Max = 2
+                    Max = GameHelper.LobbyMax()
                 };
-
-                if (menuId == 28 /* Free Play Lobby */) {
-                    ret.Party = new Party() {
-                        ID = Guid.Empty.ToString(),
-                        Size = GameHelper.LobbySize(),
-                        Max = GameHelper.LobbyMax()
-                    };
-                }
 
                 return ret;
             }
@@ -129,9 +143,16 @@ namespace PPT_RichPresence {
             Presence = new DiscordRpcClient("539426896841277440");
             //Presence.OnReady += (sender, e) => {};
             Presence.Initialize();
+
+            tray.ContextMenu.Popup += CheckFreePlayLobby;
+
             ScanTimer.Change(0, 1000);
 
-            new ManualResetEvent(false).WaitOne();
+            System.Windows.Forms.Application.Run();
+
+            ScanTimer.Dispose();
+
+            Presence.Dispose();
         }
     }
 }
